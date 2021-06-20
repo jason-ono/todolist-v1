cd471a5c712eefb669af9c6eb8c977fbe82b62d0
jason-ono <exia1999@gmail.com>
2021-06-20 14:39:25 -0400

    dynamic ejs

--- app.js
+++ app.js
@@ -8,11 +8,28 @@ const app = express();
 // integrating EJS
   // https://github.com/mde/ejs/wiki/Using-EJS-with-Express
 
+// storage for all tasks
+var items = [];
+
+// this is necessary to start parsing the body of POST request
+app.use(bodyParser.urlencoded({extended: true}));
+
 // "Use EJS as the view engine"
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
   var today = new Date();
+
+  var options = {
+    year: "numeric",
+    month: "numeric"
+  };
+
+  // day = today.toLocaleString();
+
+  /*
+  old version
+
   var day = "";
 
   if (today.getDay() === 6 || today.getDay() === 0){
@@ -20,10 +37,24 @@ app.get("/", function(req, res){
   } else {
     day = "weekday";
   }
+  */
 
+  // this is what gets passed to .ejs file
   res.render("list", {
-    dayType: day
+    dayType: today,
+    itemList: items
   });
+});
+
+// cataches the form input
+app.post("/", function(req, res){
+  var task = req.body.taskName;
+
+  // register this new task to the items
+  items.push(task);
+
+  // now, triggers app.get for home route
+  res.redirect("/");
 });
 
 app.listen(3000, function(){
